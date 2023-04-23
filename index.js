9ba585c3b36989b9d5cc07f1d60974b5e8f5449b
Taype DEV <jetaypedev@gmail.com>
2023-04-22 19:04:50 -0500

    proyecto final

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-/* 
-La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat" 
-*/
+// botones
+const formulario = document.querySelector(".form-text");
+const btnEncriptar = document.querySelector("#btn-encriptar");
+const btnDesencriptar = document.querySelector("#btn-desencriptar");
+const btnCopiar = document.querySelector("#btn-copy");
+
+// texto
+let textoUsuario = document.querySelector("#text_user");
+const textoEncriptado = document.querySelector("#text-result-success");
+
+// elementos
+const messageNotText = document.querySelector(".result-faild");
+const messageExistText = document.querySelector(".result-success");
+
+// logica
 
 const buscar = [
   { remplazar: "enter", buscar: "e" },
@@ -14,15 +22,74 @@ const buscar = [
   { remplazar: "ufat", buscar: "u" },
 ];
 
-let texto = "gato";
+/* 
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" 
+*/
 
-buscar.forEach((item, i) => {
-  texto = texto.replaceAll(item.buscar, item.remplazar);
+document.addEventListener("click", (e) => {
+  e.stopPropagation();
+  let resultado_para_usuario = textoUsuario.value;
+
+  if (e.target.id == "btn-encriptar") {
+    buscar.forEach((item) => {
+      resultado_para_usuario = resultado_para_usuario.replaceAll(
+        item.buscar,
+        item.remplazar
+      );
+    });
+
+    messageNotText.classList.add("hidden");
+    messageExistText.classList.remove("hidden");
+    textoEncriptado.textContent = resultado_para_usuario;
+  }
+
+  if (e.target.id == "btn-desencriptar") {
+    buscar.forEach((item) => {
+      resultado_para_usuario = resultado_para_usuario.replaceAll(
+        item.remplazar,
+        item.buscar
+      );
+    });
+
+    messageNotText.classList.add("hidden");
+    messageExistText.classList.remove("hidden");
+    textoEncriptado.textContent = resultado_para_usuario;
+  }
+
+  if (e.target.id == "btn-copy") {
+    navigator.clipboard.writeText(textoEncriptado.textContent);
+    textoUsuario.value = "";
+    btnEncriptar.disabled = true;
+    btnDesencriptar.disabled = true;
+  }
 });
 
-console.log(texto);
+document.addEventListener("input", (e) => {
+  function hasCapitalLetter(text) {
+    return /[A-Z]/.test(text);
+  }
 
-buscar.forEach((item, i) => {
-  texto = texto.replaceAll(item.remplazar, item.buscar);
+  let regex = /[^\w\s]/;
+
+  if (hasCapitalLetter(textoUsuario.value) || regex.test(textoUsuario.value)) {
+    btnEncriptar.disabled = true;
+    btnDesencriptar.disabled = true;
+    return;
+  }
+
+  if (textoUsuario.value.length >= 3) {
+    btnEncriptar.disabled = false;
+    btnDesencriptar.disabled = false;
+  }
+
+  if (textoUsuario.value.length < 3 || textoUsuario.value == "") {
+    btnEncriptar.disabled = true;
+    btnDesencriptar.disabled = true;
+    messageNotText.classList.remove("hidden");
+    messageExistText.classList.add("hidden");
+  }
 });
-console.log(texto);
